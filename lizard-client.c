d32b9a3ca5bacd7319ba0d445f73ba6008d1e9d1
LdeLudwig <lucasbfx@gmail.com>
2023-12-05 17:40:33 +0000

    changes at server

--- lizard-client.c
+++ lizard-client.c
@@ -1,93 +1,99 @@
-#include <stdio.h>
-#include <zmq.h>
 #include <ncurses.h>
 #include "structs.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>  
 #include <ctype.h> 
+#include <stdlib.h>
+#include <zmq.h>
 
-int main(){
+ 
 
-    //defining answer
+int main()
+{
     char answer[50];
-
-    //criando lizard
-    lizard_t lizard;
-
-    //ZeroQM context
-    void *context = zmq_ctx_new();
-    void *socket = zmq_socket(context, ZMQ_REQ);
-    zmq_connect (socket, "tcp://localhost:5555");
+    // create and open the zmq socket
+    printf ("Connecting to hello world server…\n");
+    void *context = zmq_ctx_new ();
+    void *requester = zmq_socket (context, ZMQ_REQ);
+    zmq_connect (requester, "tcp://localhost:5555");
 
     char ch;
-    //usuário definindo o char.
     do{
-        printf("Digite o caractere (a..z): ");
-        lizard.ch = getchar();
-        lizard.ch = tolower(lizard.ch);  
-    } while(!isalpha(lizard.ch));
+        printf("what is your character(a..z)?: ");
+        ch = getchar();
+        ch = tolower(ch);  
+    }while(!isalpha(ch));
+
+    lizard_t m;
+    m.msg_type = 0;
+    m.ch = ch;
+    //zmq_send (requester, &m, sizeof(m), 0);
+    //zmq_recv(requester, answer, 50, 0);
 
 
-    //adicionando letra e pontos dentro do array lizard
-    lizard.arr[0] = lizard.ch;
-    for (int i = 1; i < sizeof(lizard.arr); i++){
-        lizard.arr[i] = '.';
-    }
-
-    //mensagme de conexão
-    lizard.msg_type = 1;
-    zmq_send(socket, &lizard, sizeof(lizard_t), 0);
-    zmq_recv(socket, answer, sizeof(answer), 0);
-    
 	initscr();			/* Start curses mode 		*/
 	cbreak();				/* Line buffering disabled	*/
 	keypad(stdscr, TRUE);		/* We get F1, F2 etc..		*/
 	noecho();			/* Don't echo() while we do getch */
-   
 
-    //definindo tipo de mensagem para movimento
-    lizard.msg_type = 1;
-    
-    int ch;
     int n = 0;
+
+    //TODO_9
+    // prepare the movement message
+    m.msg_type = 1;
+    m.ch = ch;
+    
+    int key;
     do
     {
-    	ch = getch();		
+    	key = getch();		
         n++;
-        switch (ch)
-        // definindo direção para o ncurses
+        switch (key)
         {
-            case KEY_LEFT:
-                mvprintw(0,0,"%d Left arrow is pressed", n);
-                lizard.direction = LEFT;
-                break;
-            case KEY_RIGHT:
-                mvprintw(0,0,"%d Right arrow is pressed", n);
-                lizard.direction = RIGHT;
-                break;
-            case KEY_DOWN:
-                mvprintw(0,0,"%d Down arrow is pressed", n);
-                lizard.direction = DOWN;
-                break;
-            case KEY_UP:
-                mvprintw(0,0,"%d Up arrow is pressed", n);
-                lizard.direction = UP;
-                break;
-            default:
-                ch = 'x';
-                    break;
+        case KEY_LEFT:
+            mvprintw(0,0,"%d Left arrow is pressed", n);
+            //TODO_9
+            // prepare the movement message
+           m.direction = LEFT;
+            break;
+        case KEY_RIGHT:
+            mvprintw(0,0,"%d Right arrow is pressed", n);
+            //TODO_9
+            // prepare the movement message
+            m.direction = RIGHT;
+            break;
+        case KEY_DOWN:
+            mvprintw(0,0,"%d Down arrow is pressed", n);
+            //TODO_9
+            // prepare the movement message
+           m.direction = DOWN;
+            break;
+        case KEY_UP:
+            mvprintw(0,0,"%d :Up arrow is pressed", n);
+            //TODO_9
+            // prepare the movement message
+            m.direction = UP;
+            break;
+
+        default:
+            key = 'x'; 
+            break;
         }
-        refresh(); 
 
-        //TODO_9
-        // prepare the movement message
-        if (ch != 'x'){
-            zmq_send(socket, &lizard, sizeof(lizard_t), 0);
-            zmq_recv(socket, answer, sizeof(answer), 0);
+        //TODO_10
+        //send the movement message
+        if (key != 'x'){
+            zmq_send(requester, &m, sizeof(m), 0);
+            zmq_recv(requester, answer, 3, 0);  
         }
-        
-    }while(ch != 81);
-
-     endwin();
-    zmq_close(socket);
-    zmq_ctx_destroy(context);
-
+        refresh();			/* Print it on to the real screen */
+    }while(key != 27);
+    
+    
+  	endwin();			/* End curses mode		  */
+    zmq_close(requester);
+    zmq_close(context);
+	return 0;
 }
